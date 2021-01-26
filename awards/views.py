9a33b6adff6968c45d01999a4a561163a3021f1d
Arisodee <arisodee@gmail.com>
2021-01-26 18:14:33 +0300

    add post request function to projects  API view

--- awards/views.py
+++ awards/views.py
@@ -203,7 +203,7 @@ def logout_request(request):
 
 class ProfileList(APIView):
     def get(self,request,format = None):
-        all_profiles = Profile.objects.all()
+        all_profile = Profile.objects.all()
         serializers = ProfileSerializer(all_profiles,many = True)
         return Response(serializers.data)
 
@@ -212,3 +212,11 @@ class ProjectList(APIView):
         all_projects = Projects.objects.all()
         serializers = ProjectSerializer(all_projects,many = True)
         return Response(serializers.data)
+
+    
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
